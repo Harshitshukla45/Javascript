b326f6f8fa297dcf7c78155b46268f1d6ff4d176
HARSHIT SHUKLA <91362502+Harshitshukla45@users.noreply.github.com>
2022-10-07 19:58:40 +0530

    Create javascript in kerel .js

---
+++ javascript in kerel .js
@@ -0,0 +1,19 @@
+function main(){
+   //your code here
+   putBeepers();
+   putBeepers();
+   putBeepers();
+   putBeepers();
+   putBeeper();
+}
+function putBeepers()
+{
+   putBeeper();
+   move();
+   turnLeft();
+   move();
+   turnLeft();
+   turnLeft();
+   turnLeft();
+   
+}
